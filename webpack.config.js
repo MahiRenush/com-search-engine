180d4cceb0394de31f43cd37f741fdbf9a9c286d
RenushA, MahipasAmala <MahipasAmala_RenushA@comcast.com>
2022-03-14 23:12:56 +0530

    feat(webpack): start base setup by running dev server

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.?css$/,
+                use: ["style-loader", "css-loader" ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "index.html")
+        })
+    ]
+}
